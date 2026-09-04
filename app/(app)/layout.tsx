709d22c11105a2fc09e3486e1594a9a5afa08c61
Tharun4743 <tharunkumark42007@gmail.com>
2026-09-04 10:08:07 +0530

    style: optimize layout width for 16:9 widescreen viewports

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -145,7 +145,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           </div>
         )}
 
-        <main className="flex-1 min-w-0 p-6 md:p-8 lg:p-10 max-w-5xl">
+        <main className="flex-1 min-w-0 p-6 md:p-8 lg:p-10 w-full max-w-7xl mx-auto">
           {children}
         </main>
       </div>
